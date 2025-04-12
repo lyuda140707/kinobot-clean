208d126ff5ec7747cffe41b21b6d7ede7266b680
Людмила <lyuda14070702@icloud.com>
2025-04-12 20:41:04 +0300

    fix port binding for render

--- app.py
+++ app.py
@@ -214,4 +214,5 @@ async def root():
     return {"status": "OK"}
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
